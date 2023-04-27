f497be716a821f57dc3402f7075be66e3cbb80e2
MineExplorer <vladgrz40@gmail.com>
2023-04-27 18:22:28 +0300

    Added YoptaC headers and README

---
+++ YoptaC.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+// синтаксис
+#define жџ {
+#define всЄ }
+#define все }
+#define нах ;
+#define нахуй ;
+#define не !
+#define и &&
+#define или ||
+#define пизже >
+#define хуЄвей <
+#define хуевей <
+#define нехуЄвей >=
+#define нехуевей >=
+#define непизже <=
+#define типо ==
+#define ровно ==
+#define внатуре ==
+#define конкретно ===
+#define неровно !=
+#define тру true
+#define чЄтко true
+#define четко true
+#define чотко true
+#define нетру false
+#define пиздишь false
+#define нечЄтко false
+#define нечетко false
+#define нечотко false
+#define хули if
+#define вилкойвглаз if
+#define огребаешь else
+#define иливжопураз else
+#define крч do
+#define потрещим while
+#define го for
+#define харэ break
+#define двигай continue
+#define естьчо switch
+#define аеслинайду case
+#define отвечаю return
+#define зайти_на using
+#define район namespace
+#define размер sizeof
+// ввод-вывод
+#define слышь scanf
+#define сказать printf
+#define говори std::cin
+#define базарить std::cout
+#define чЄ >>
+#define че >>
+#define чо >>
+#define бл€ <<
+#define бл€ть <<
+#define конец std::endl
+#define русский SetConsoleCP(1251)
+#define крымнаш SetConsoleOutputCP(1251)
+// матан
+#define шара rand
+#define рандом rand
+#define абсолютли abs
+#define корень sqrt
+#define гопень pow
+#define гопспанента exp
+#define гопорифм log
+#define гопорифмѕо10 log10
+
+#define €сен’уй const
+typedef void куку;
+typedef bool поц;
+typedef char гыы;
+typedef short пипин;
+typedef int чЄткий;
+typedef unsigned int натурал;
+typedef long long длинный;
+typedef float число;
+typedef double двойной;
+typedef std::string букавы;
